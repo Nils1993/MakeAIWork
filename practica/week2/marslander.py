94e30465316fa22df0e774226d3978ae392c1c6a
Nils1993 <10696960@hr.nl>
2023-02-15 16:33:49 +0100

    update

---
+++ practica/week2/marslander.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class marslander:
+    length: int
+    width: float
+    weight: int
+    deckHeight: int
+    robotArmLength: float
+    numberOfSolarPanels: int
