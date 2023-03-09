a8a6c03e388a924e171606f68cab124f2206cb5f
Nils1993 <10696960@hr.nl>
2023-03-09 16:20:47 +0100

    testing_new_data_on_multiple_linear_regression

---
+++ projects/project1/scripts/prototype1.py
@@ -0,0 +1,14 @@
+# PROJECT 1: Creating a program that can predict someone's maximum age.
+
+# Installing libraries
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+
+
+
