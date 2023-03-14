3212e5da3c19e0f59716ca1338dc3e07b0715a8d
Nils1993 <10696960@hr.nl>
2023-03-14 09:05:53 +0100

    training_models_and_pyscript_start

--- projects/project1/scripts/prototype1.py
+++ projects/project1/scripts/prototype1.py
@@ -1,14 +1,25 @@
+#!/usr/bin/env python
 # PROJECT 1: Creating a program that can predict someone's maximum age.
 
 # Installing libraries
+import logging
 import pickle
 import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+
+# LOG LEVELS
+# DEBUG		Detailed information, useful during bugfixing
+# INFO		Just enough information to be able to understand the flow
+# WARNING	Information about unexpected conditions that might be solved later
+# ERROR		Reporting that the program can perform some task
+# CRITICAL	Reporting that the program can not continue
+
+# logging info
+logging.basicConfig(level=logging.DEBUG)
+
 
 # Load my model
 with open('C:/Users/nilsm/MakeAIWork/projects/project1/pickle/lifespan_model.pkl', 'rb') as f:
@@ -40,4 +51,8 @@ def lifespan():
 
     print(f'Your predicted age IF nothing changes in your lifestyle is {span} years')
 
+# Run the function to make the prediction
 lifespan()
+
+# logging
+logging.info("Logging to file..")
